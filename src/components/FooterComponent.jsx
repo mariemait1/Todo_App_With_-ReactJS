f0d77cbc1a6df075810b7ebd4923634a89fcd5a8
Mariem <mariem.aithou@gmail.com>
2024-03-27 16:03:19 +0000

    First Commit

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <p className='text-center'>
+                Copyrights reserved at 2024-26 By Mariem
+            </p>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
